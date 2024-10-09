dccb5de6d783a992a744fd0e1b273094687ee7ba
José Junior <jzjuniorbrito@gmail.com>
2024-10-09 09:36:51 -0300

    commitando 1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue'; // Import correto
+
+export default {
+  components: { Banner, ConteudoPrincipal }
+}
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+</template>
